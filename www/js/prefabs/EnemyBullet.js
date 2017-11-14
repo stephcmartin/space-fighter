57312b8f74b24bd143e12976276824acce431699
stephcmartin <martin.stephaniechow@gmail.com>
2017-11-14 18:51:07 -0500

    Enemy now shots bullets at player
    - Added new prefabs file for EnemyBullet
    - Added behavior of bullets
    - Game now restarts when enemy's bullet hits player

---
+++ www/js/prefabs/EnemyBullet.js
@@ -0,0 +1,14 @@
+var SpaceHipster = SpaceHipster || {};
+
+// inherits from phaser.sprite class object
+SpaceHipster.EnemyBullet = function(game, x, y){
+    Phaser.Sprite.call(this, game, x, y, 'bullet');
+    // ^ This means when we create a player, bullet needs to be created with the corresponding cords.
+    this.anchor.setTo(0.5);
+    // Check if the sprite is out of the bounds of the world, if it is then kill it.
+    this.checkWorldBounds = true;
+    this.outOfBoundsKill = true;
+};
+// creating an object that is similar or the same type as the prototype of the sprite
+SpaceHipster.EnemyBullet.prototype = Object.create(Phaser.Sprite.prototype);
+SpaceHipster.EnemyBullet.prototype.constructor = SpaceHipster.EnemyBullet;
